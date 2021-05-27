3dd33dd0e74cf8c6df182a1253855a763bf91a4d
BasBijkerk <78425828+BasBijkerk@users.noreply.github.com>
2021-05-27 04:02:28 -0700

    Add files via upload

---
+++ FibbonachiEvenMachine/FibbonachiEvenMachine/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace FibbonachiEvenMachine
+{
+    class Program
+    {
+        static int Endval = 0;
+        static int firstval = 1;
+        static int secondval = 2;
+        static int calcval = 0;
+        static int calcresult = 2 ;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Fibbonachi even machine");
+            Console.WriteLine("Please enter End Value(Number below wich the program will keep doing calculations)");
+            Endval = Convert.ToInt32(Console.ReadLine());
+            while(calcval < Endval)
+            {
+                calcval = firstval + secondval;
+                firstval = secondval;
+                secondval = calcval;
+                if(calcval%2 == 0 && calcval < Endval)
+                {
+                    Console.WriteLine("Even Number: " + calcval);
+                    calcresult += calcval;
+                }
+                
+            }
+            Console.WriteLine("Result: " + calcresult);
+        }
+    }
+}
